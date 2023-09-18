4ccd066d7c448c6cb9bc15b763aa1638c712a6db
Oleksandr Taruraiev <oleksandr_taruraiev@epam.com>
2023-09-18 17:52:12 +0300

    [EPMDEDP-12554]:feat: Migrate clean-edp-helm-charts Pipeline from Jenkins to GitHub Actions

---
+++ .github/workflows/helm_delete_old_packages.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from collections import defaultdict
+
+def delete_old_packages(directory):
+    helmpath = Path(directory)
+    helmpackages = list(helmpath.glob("*.tgz"))
+
+    package_versions = defaultdict(list)
+    for package in helmpackages:
+        package_name = package.name.rsplit(".", 2)[0]
+        package_versions[package_name].append(package)
+
+    packages_to_delete = [pkg for pfiles in package_versions.values() for pkg in sorted(pfiles, key=lambda x: x.name.split('.')[-2], reverse=True)[5:]]
+
+    for ptd in packages_to_delete:
+        os.remove(ptd)
+
+def process_repositories():
+    delete_old_packages("snapshot/packages")
+
+if __name__ == "__main__":
+    process_repositories()
