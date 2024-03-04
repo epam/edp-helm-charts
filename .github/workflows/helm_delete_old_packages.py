29c76b1f8d0a0ce82a743a4f247f3d22dc46119e
Taruraiev Oleksandr <77995538+alezander86@users.noreply.github.com>
2024-03-04 13:52:46 +0200

    [EPMDEDP-13376]: fix: Align pattern for removing old packages

--- .github/workflows/helm_delete_old_packages.py
+++ .github/workflows/helm_delete_old_packages.py
@@ -8,12 +8,21 @@ def delete_old_packages(directory):
     helmpath = Path(directory)
     helmpackages = list(helmpath.glob("*.tgz"))
 
+    version_modifiers = {'rc': 1, 'snapshot': 2}
+
+    def get_version_num(file):
+        version_part = file.name.rsplit("-", 2)[-1].split('.')[:-1]
+        if 'rc' in map(str.lower, version_part):
+            return None
+        return [int(num) if num.isdigit() else version_modifiers.get(num.lower(), 999) for num in version_part]
+
     package_versions = defaultdict(list)
     for package in helmpackages:
-        package_name = package.name.rsplit(".", 2)[0]
-        package_versions[package_name].append(package)
+        package_name = '-'.join(package.name.rsplit("-", 2)[:-1])
+        if get_version_num(package) is not None:
+            package_versions[package_name].append(package)
 
-    packages_to_delete = [pkg for pfiles in package_versions.values() for pkg in sorted(pfiles, key=lambda x: x.name.split('.')[-2], reverse=True)[MAX_COPIES_RESERVED:]]
+    packages_to_delete = [pkg for pfiles in package_versions.values() for pkg in sorted(pfiles, key=get_version_num, reverse=True)[MAX_COPIES_RESERVED:]]
 
     for ptd in packages_to_delete:
         os.remove(ptd)
